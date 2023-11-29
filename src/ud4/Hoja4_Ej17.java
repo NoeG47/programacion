06167c1a75ca23f8811c8e34c21536444c70eefa
noe <noe@1DAW-13>
2023-11-29 12:03:18 +0100

    primer commit

--- src/ud4/Hoja4_Ej17.java
+++ src/ud4/Hoja4_Ej17.java
@@ -9,70 +9,56 @@ public class Hoja4_Ej17 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int matriz[][] = new int[10][11];
-		int matrizT[][] = new int[11][10];
+	
 		//int sumaColumnas[] = new int[10];
-		matriz(matriz);
-		matrizT(matriz, matrizT);
-		sumaMatrizT(matrizT);
+		rellenamatriz(matriz);
+		for (int i = 0; i<10; i++) {
+			sumaMatrizT(matriz[i]);
+		}
 		muestraMatriz(matriz);
-		//muestraArray(sumaColumnas);
+		
+		
 		
 	}
 
 	
-	public static int[][] matriz(int matriz[][]) {
+	public static void rellenamatriz(int matriz[][]) {
 		int num = 1;
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				// System.out.println("Introduce numeros");
 				// matriz[i][j] = entrada.nextInt();
-				matriz[i][j] = num;
+				matriz[j][i] = num;
 				num++;
 			}
 		}
-		return matriz;
 	}
 
-//calculamos la  transpuesta y lo almacenamos
-	public static int[][] matrizT(int matriz[][], int matrizT[][]) {
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz[i].length; j++) {
-				matrizT[j][i] = matriz[i][j];
-			}
 
-		}
-		return matrizT;
-	}
 
 	
-	public static void sumaMatrizT(int matriz[][]) {
-		for (int i = 0; i < 10; i++) {
+	public static void sumaMatrizT(int [] vector) {
 			int suma = 0;
-			for (int j = 0; j < 10; j++) {
-				suma += matriz[j][i];
+			for (int i = 0; i < 10; i++) {
+				suma = suma + vector[i];
 			}
 			//sumaColumnas[i] = suma;
-			matriz[i][10] = suma; // Almacena la suma en la última fila de la columna i en la matriz
+			vector[10] = suma; // Almacena la suma en la última fila de la columna i en la matriz
 													// transpuesta
-		}
+		
 	}
-
-	
-	public static void muestraMatriz(int matriz[][]) {
+	public static void muestraMatriz(int [][]  matriz) {
 		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 11; j++) {
-				System.out.print(matriz[i][j] + " ");
+			for (int j = 0; j < 10; j++) {
+				System.out.print(matriz[j][i] + " ");
 			}
-			System.out.println();
+		System.out.println();	
+		}
+		for (int i= 0; i< 10; i++) {
+			System.out.print(matriz[i][10] + " ");
 		}
 	}
-
-	/*public static void muestraArray(int array[]) {
-		for (int i = 0; i < array.length; i++) {
-			System.out.print(array[i] + " ");
-		}
-		System.out.println();
-	}*/
+	
 	
 	
 }
